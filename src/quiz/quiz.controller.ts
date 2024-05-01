eb6c1b5d962d04705ca5051785df2196327f27a0
jeemh <jimyeongheon@gmail.com>
2024-05-01 22:01:12 +0900

    style: 필요없는 주석/콘솔출력 제거

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -32,58 +32,17 @@ export class QuizController {
       return null;
     }
   }
-  /*요청: 
-GET  /api/quizsets?subLectureUrl={subLectureUrl}&mainLectureTitle={mainLectureTitle}&subLectureTitle={subLectureTitle}
-Query
-subLectureUrl(강의 url), subLectureTitle(소강의명), 
-mainLectureTitle(대강의명)
 
-반환:
-[문제집명, 문제집 작성자, 
-추천수, 작성일자]
-
-@Get('')
-들어오는 데이터: 멤버id
-리턴해야할 데이터:
-[
-  문제집명(quiz_sets title), 
-  소강의명(sub_lectures title),
-  등록자 닉네임(members nickname),
-  작성일자(quiz_sets created_at),
-  추천수(recommendations quiz_set_id 갯수)
-]
-*/
   @Get('/')
   async readQuiz(@Query('subLectureUrl') subLectureUrlEncoded: string) {
     if (subLectureUrlEncoded) {
       const subLectureUrl = decodeURIComponent(subLectureUrlEncoded);
-      console.log('subLectureUrl: ', subLectureUrl);
       return await this.quizService.readCertainQuizSets(subLectureUrl);
     } else {
       return await this.quizService.readQuizSet();
     }
   }
 
-  /*
-  요청:
-GET   /api/quizsets/{quizsetsId}/quizzes?choices={}&commentary={}   
-Path & Query
-quizsetsId : 문제집의 id, commentary(해설 제공여부),
-choices(선택지 제공여부)
-(제공 여부는 true or false이다.)
-
-응답: 
-[
-  문제, 
-  해설,
-  팝업 시각, 
-  [
-    선택지id,
-    내용,
-    정답 여부
-  ]
-]
-*/
   @Get('/:quizsetId/quizzes')
   async getQuizDetails(
     @Param('quizsetId') quizsetId: number,
@@ -97,26 +56,6 @@ choices(선택지 제공여부)
     );
   }
 
-  /* 
-@Post('')
-body에 들어오는 데이터:
-
-mainLectureTitle(대강의명), main_lectures 
-lecturerName(강사명), main_lectures
-subLectureUrl(강의 url), sub_lectures 1
-subLectureTitle(소강의명), sub_lectures
-duration(강의시간, 초단위 정수), sub_lectures
-title(문제집명), quiz_sets 1
-[
-    instruction(문제), quizzes 2
-    commentary(해설), quizzes 2
-    popupTime(팝업시각, 'hh:mm:ss'), quizzes 1
-    [
-        content(선택지), choices 2
-        isAnswer(정답여부) choices 2
-    ]
-]
-*/
   @Post('')
   async createQuiz(
     @Body() quizInfo: CreateQuizSetDTO,
