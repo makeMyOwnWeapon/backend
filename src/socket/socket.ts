2f36c729cd872e10ad89b6f263f4cc862a17d337
koesnam <76582376+10kseok@users.noreply.github.com>
2024-05-07 22:53:05 +0900

    styel: eslint 적용

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,4 +1,10 @@
-                                                              import { WebSocketGateway, SubscribeMessage, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { JwtService } from '@nestjs/jwt';
 import { EventEmitter2 } from '@nestjs/event-emitter';
@@ -7,7 +13,10 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private hashTable = new Map();
 
-  constructor(private jwtService: JwtService, private eventEmitter: EventEmitter2) {}
+  constructor(
+    private jwtService: JwtService,
+    private eventEmitter: EventEmitter2,
+  ) {}
 
   @WebSocketServer() server: Server;
 
@@ -22,9 +31,15 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('sendData')
   Hashing(client: Socket, data: any) {
     const MemberID = this.jwtService.decode(data.token).id;
-    const connectionInfo = { socketId: data.socketId, subLectureId: data.subLectureId };
+    const connectionInfo = {
+      socketId: data.socketId,
+      subLectureId: data.subLectureId,
+    };
     this.hashTable.set(MemberID, connectionInfo);
-    this.eventEmitter.emit('member.connection', { MemberID, ...connectionInfo });
+    this.eventEmitter.emit('member.connection', {
+      MemberID,
+      ...connectionInfo,
+    });
     console.log('Hash Table Updated:', this.hashTable);
   }
 
