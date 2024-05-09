23ef653652232ea5aa48716e12ae53c9bf0f7eff
jeemh <jimyeongheon@gmail.com>
2024-05-10 00:50:24 +0900

    feat: 회원레포트 익스텐션에서 볼 수 있도록 엔드포인트 추가

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -6,6 +6,7 @@ import { ReadHistoriesDTO } from './dto/readHistories.dto';
 import { ReadHistoryReportDTO } from './dto/readHistoryReport.dto';
 import { UserRequest } from '../auth/UserRequest';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Public } from 'src/auth/auth.guard';
 
 @ApiTags('histories')
 @Controller('history')
@@ -21,6 +22,28 @@ export class HistoryController {
     description: '회원레포트 목록/상세 조회',
   })
   async readHistories(
+    @Req() req: UserRequest,
+    @Query('subLectureId') subLectureId: number,
+    @Query('lectureHistoryId') lectureHistoryId: number,
+  ): Promise<ReadHistoryReportDTO | ReadHistoriesDTO[]> {
+    const memberId = req.user.id;
+    if (subLectureId) {
+      const quizzes = await this.quizService.retrieveQuizEntity(subLectureId);
+      return await this.historyService.readHistoryReport(
+        lectureHistoryId,
+        memberId,
+        quizzes,
+      );
+    } else {
+      return await this.historyService.readHistories(memberId);
+    }
+  }
+  @Public()
+  @Get('/extension')
+  @ApiOperation({
+    summary: '회원레포트 익스텐션에서 목록/상세 조회',
+  })
+  async readHistoriesInExtension(
     @Req() req: UserRequest,
     @Query('subLectureId') subLectureId: number,
     @Query('lectureHistoryId') lectureHistoryId: number,
