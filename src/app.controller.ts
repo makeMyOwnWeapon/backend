900d6b28e2de439474cacfe9f5f85d81cd1dceb9
koesnam <76582376+10kseok@users.noreply.github.com>
2024-05-11 01:32:44 +0900

    feat: 영상 처리 API 구현
    이미지 파일을 받아 분석 결과를 졸음과 자리이탈 결과를 반환합니다.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express/multer';
+import { Controller, Get } from '@nestjs/common';
 import * as process from 'process';
 import { Public } from 'src/auth/auth.guard';
-import { spawn } from 'child_process';
-
-export class AnalysisResultType {
-  isBlank: boolean;
-  isSleep: boolean;
-}
 
 @Controller()
 export class AppController {
@@ -21,40 +8,5 @@ export class AppController {
   @Get('/env')
   getEnv(): string {
     return process.env.NODE_ENV;
-  }
-
-  @Public()
-  @Post('/video')
-  @UseInterceptors(FileInterceptor('file'))
-  async analyzeVideo(@UploadedFile() file: Express.Multer.File) {
-    console.log('client file: ', file);
-    const result = await this.executePyFile(file.buffer.toString('base64'));
-    return { result: result };
-  }
-
-  executePyFile(base64ImageData: string): Promise<AnalysisResultType> {
-    return new Promise((resolve, reject) => {
-      const pythonProcess = spawn('python3', ['./mediapipe_analysis.py']);
-      let dataString = '';
-      pythonProcess.stdout.on('data', (data) => {
-        dataString += data.toString();
-      });
-      pythonProcess.stderr.on('data', (data) => {
-        console.error(`stderr: ${data.toString()}`);
-      });
-      pythonProcess.on('error', (error) => {
-        reject(error);
-      });
-      pythonProcess.on('close', (code) => {
-        if (code !== 0) {
-          reject(new Error(`Python process exited with code ${code}`));
-        } else {
-          resolve(JSON.parse(dataString));
-        }
-      });
-      // 데이터 전송
-      pythonProcess.stdin.write(base64ImageData);
-      pythonProcess.stdin.end();
-    });
   }
 }
