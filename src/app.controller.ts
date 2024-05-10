f10af56503bcdf6e6d51106c088eb2054fc3eb2c
koesnam <76582376+10kseok@users.noreply.github.com>
2024-05-10 16:01:23 +0900

    test: 이미지 파일을 받아 py 파일로 전달

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express/multer';
 import * as process from 'process';
 import { Public } from 'src/auth/auth.guard';
+import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,31 @@ export class AppController {
   @Get('/env')
   getEnv(): string {
     return process.env.NODE_ENV;
+  }
+
+  @Public()
+  @Post('/video')
+  @UseInterceptors(FileInterceptor('file'))
+  async analyzeVideo(@UploadedFile() file: Express.Multer.File) {
+    console.log('client file: ', file);
+    const result = await this.executePyFile(file.buffer.toString('base64'));
+    return { message: 'upload success', result: result };
+  }
+
+  executePyFile(base64ImageData: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      exec(
+        `python3 ./mediapipe.py ${base64ImageData}`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return reject(error);
+          }
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+          resolve(stdout);
+        },
+      );
+    });
   }
 }
