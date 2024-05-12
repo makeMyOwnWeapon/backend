4f2658c4e3beff25652d7effc1369afe7a977064
koesnam <76582376+10kseok@users.noreply.github.com>
2024-05-12 20:03:28 +0900

    refactor: 영상처리 결과 JSON으로 반환
    String 형식으로 반환되던 응답을 JSON으로 반환합니다.

--- image_processing_server/main.py
+++ image_processing_server/main.py
@@ -1,20 +1,17 @@
 import base64
-from typing import Annotated, Union
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 
 import mediapipe_analysis
 
 app = FastAPI()
 
+async def process_image(image_data):
+    base64_encoded_data = base64.b64encode(image_data)
+    base64_encoded_str = base64_encoded_data.decode('utf-8')
+    return mediapipe_analysis.checkImage(base64_encoded_str)
+
 @app.post("/api/image-process/image")
 async def create_file(file: UploadFile):
-    image_data = await file.read()
-    # 읽은 이미지 데이터를 base64로 인코딩합니다.
-    base64_encoded_data = base64.b64encode(image_data)
-    
-    # base64_encoded_data는 bytes이므로, 이를 문자열로 변환합니다.
-    base64_encoded_str = base64_encoded_data.decode('utf-8')
-    
-    # 수정된 부분: base64 문자열을 mediapipe_analysis.checkImage에 전달합니다.
-    return mediapipe_analysis.checkImage(base64_encoded_str)
+    result = await process_image(await file.read())
+    return result
