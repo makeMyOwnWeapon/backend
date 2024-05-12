6382c6582a069fd4decae98a8e096c8c78565278
koesnam <76582376+10kseok@users.noreply.github.com>
2024-05-12 20:27:20 +0900

    refactor: 영상 처리 속도 100% 개선
    매 요청시마다 모델을 불러오는 것을 초기화시 한번만 불러오는 방식으로 변경 (local 기준, 평균 60ms -> 30ms)

--- image_processing_server/main.py
+++ image_processing_server/main.py
@@ -9,9 +9,13 @@ app = FastAPI()
 async def process_image(image_data):
     base64_encoded_data = base64.b64encode(image_data)
     base64_encoded_str = base64_encoded_data.decode('utf-8')
-    return mediapipe_analysis.checkImage(base64_encoded_str)
+    return mediapipe_analysis.check_image(base64_encoded_str)
 
 @app.post("/api/image-process/image")
 async def create_file(file: UploadFile):
     result = await process_image(await file.read())
     return result
+
+@app.get("/api/image-process/health-check")
+def create_file():
+    return { "message" : 'OK' }
--- image_processing_server/mediapipe_analysis.py
+++ image_processing_server/mediapipe_analysis.py
@@ -4,11 +4,12 @@ from mediapipe.tasks.python import vision
 import cv2
 import base64
 import numpy as np
-import json
 import sys
 import os
 
-def base64ToImage(base64_string):
+detector = None
+
+def base64_to_image(base64_string):
     # Base64 문자열을 바이트 배열로 디코딩
     imgdata = base64.b64decode(base64_string)
     
@@ -21,19 +22,25 @@ def base64ToImage(base64_string):
     # 이미지를 RGB로 변환 (mediapipe가 RGB를 사용함)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def checkImage(base64_string):
-    current_file_path = os.path.abspath(__file__)
-    current_dir_path = os.path.dirname(current_file_path)
-    # STEP 2: Create an FaceLandmarker object.
-    base_options = python.BaseOptions(model_asset_path=f'{current_dir_path}/face_landmarker_v2_with_blendshapes.task')
-    options = vision.FaceLandmarkerOptions(base_options=base_options,
-                                        output_face_blendshapes=True,
-                                        output_facial_transformation_matrixes=True,
-                                        num_faces=1)
-    detector = vision.FaceLandmarker.create_from_options(options)
+def get_detector():
+    global detector
+    if detector == None:
+        current_file_path = os.path.abspath(__file__)
+        current_dir_path = os.path.dirname(current_file_path)
+        # STEP 2: Create an FaceLandmarker object.
+        base_options = python.BaseOptions(model_asset_path=f'{current_dir_path}/face_landmarker_v2_with_blendshapes.task')
+        options = vision.FaceLandmarkerOptions(base_options=base_options,
+                                            output_face_blendshapes=True,
+                                            output_facial_transformation_matrixes=True,
+                                            num_faces=1)
+        detector = vision.FaceLandmarker.create_from_options(options)
+    return detector
+
+def check_image(base64_string):
+    detector = get_detector()
 
     # STEP 3: Load the input image.
-    image_rgb = base64ToImage(base64_string)
+    image_rgb = base64_to_image(base64_string)
 
     # STEP 4: Convert the image to Mediapipe format.
     image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
@@ -41,23 +48,23 @@ def checkImage(base64_string):
     # STEP 5: Detect face landmarks from the input image.
     detection_result = detector.detect(image)
 
-    isExist = True
-    isEyeClosed = False
+    is_exist = True
+    is_eye_closed = False
     if(detection_result.face_blendshapes):
         # 눈감음
         if(detection_result.face_blendshapes[0][9].score > 0.4500 and detection_result.face_blendshapes[0][10].score > 0.4500):
-            isEyeClosed = True
+            is_eye_closed = True
     else:
         # 자리이탈
-        isExist = False
+        is_exist = False
     
     result = {
-        "isExist" : isExist,
-        "isEyeClosed" : isEyeClosed
+        "isExist" : is_exist,
+        "isEyeClosed" : is_eye_closed
     }
     return result
 
 if __name__ == "__main__":
     base64_image_data = sys.stdin.read() # Node.js에서 받아온 데이터
-    result = checkImage(base64_image_data)
+    result = check_image(base64_image_data)
     print(result)
